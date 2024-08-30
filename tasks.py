f79fc0b11c905145f1092dfaa85370e87f14cd95
Clément Zitouni <92778930+cazitouni@users.noreply.github.com>
2024-08-30 09:32:18 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -18,7 +18,7 @@ def fetch_parcelle_data(parcelle, token):
     }
     if len(parcelle[0:5]) != 5:
         return 404
-    response = requests.get(f'https://apidf-preprod.cerema.fr/ff/geoparcelles?code_insee={parcelle[0:5]}&fields=all&idpar={parcelle}', headers=headers)
+    response = requests.get(f'https://apidf-preprod.cerema.fr/ff/geoparcelles?code_insee={parcelle[0:5]}&fields=all&idpar={parcelle}', headers=headers, timeout=5)
     if response.status_code == 401:
         return 401
     elif response.status_code == 403:
@@ -28,6 +28,6 @@ def fetch_parcelle_data(parcelle, token):
     parcelle_data = response.json()
     if not parcelle_data or 'features' not in parcelle_data or not parcelle_data['features']:
         return 404
-    proprietaire_data = requests.get(f'https://apidf-preprod.cerema.fr/ff/proprios?code_insee={parcelle[0:5]}&fields=all&idprocpte={parcelle_data["features"][0]["properties"]["idprocpte"]}', headers=headers).json()
-    locaux_data = requests.get(f'https://apidf-preprod.cerema.fr/ff/locaux?code_insee={parcelle[0:5]}&fields=all&idprocpte={parcelle_data["features"][0]["properties"]["idprocpte"]}', headers=headers).json()
+    proprietaire_data = requests.get(f'https://apidf-preprod.cerema.fr/ff/proprios?code_insee={parcelle[0:5]}&fields=all&idprocpte={parcelle_data["features"][0]["properties"]["idprocpte"]}', headers=headers, timeout=5).json()
+    locaux_data = requests.get(f'https://apidf-preprod.cerema.fr/ff/locaux?code_insee={parcelle[0:5]}&fields=all&idprocpte={parcelle_data["features"][0]["properties"]["idprocpte"]}', headers=headers, timeout=5).json()
     return create_pdf(parcelle_data['features'][0], proprietaire_data.get("results", []), locaux_data.get("results", []))
